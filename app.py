1492c2d1cb7d5b8eccdbdf2965aa03cf5f82d9b5
hackermbrebed <48652196+hackermbrebed@users.noreply.github.com>
2025-11-11 13:23:25 +0700

    updt
    Add Flask application setup with create_app function.

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-.
+from flask import Flask
+
+# Fungsi untuk membuat dan mengkonfigurasi aplikasi
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'kunci-rahasia-anda' 
+    
+    # Import dan registrasi Blueprint (akan kita tambahkan nanti)
+    # from .website import views
+    # app.register_blueprint(views.bp)
+
+    return app
+
+if __name__ == '__main__':
+    # Jalankan aplikasi (mode debug=True selama pengembangan)
+    app = create_app()
+    # Pastikan Anda menginstal Flask: pip install Flask
+    app.run(debug=True)
